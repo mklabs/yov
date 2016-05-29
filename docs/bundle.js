d78726f06d57b79fb4668fc852ff2cb47b237b6b
Mickael Daniel <daniel.mickael@gmail.com>
2016-05-29 07:50:34 +0200

    Cleanup docs folder from generated content

--- docs/bundle.js
+++ docs/bundle.js
@@ -2,7 +2,7 @@ const debug = require('debug');
 debug.enable('yoview:*');
 
 const Layout = require('./layout');
-const Textarea = require('./textarea');
+const Form = require('./form');
 
 let app = document.querySelector('.js-main');
 
@@ -30,13 +30,26 @@ function init () {
 }
 
 function textarea (layout) {
-  let json = { foo: 'bar' };
-  let txt = window.txt = new Textarea({
-    defaults: { json }
-  });
+  let form = window.form = new Form();
+  layout.set('content', form.el);
+  form.set('json', form.data);
 
-  layout.set('content', txt.el);
-  return txt;
+  form.on('change:editor', update.bind(null, layout, form));
+}
+
+function update (view, form, json) {
+  let data = {};
+  try {
+    data = JSON.parse(json);
+  } catch (e) {
+    view.debug('Invalid JSON: %s', e.message);
+    view.debug(e.stack);
+  }
+
+  // todo: fix that
+  // quick hack to update the nested el, have to deal with it in the View component.
+  view.demo.el = document.querySelector('.js-demo');
+  view.demo.set(data);
 }
 
 init();
--- docs/layout.js
+++ docs/layout.js
@@ -1,15 +1,37 @@
 const View = require('..');
 
+const Demo = require('./demo');
+
 export default class Layout extends View {
+
+  get style () {
+    return `
+    .mdl-layout .mdl-layout__header-row {
+      padding-left: 20px;
+    }`;
+  }
+
+  constructor () {
+    super();
+
+    this.demo = new Demo();
+    this.injectCSS(this.style);
+  }
+
+  demoEl (props) {
+    this.demo = this.demo || new Demo();
+    return this.demo.template(props);
+  }
+
   template (props) {
     let { content } = props;
     content = content || '';
 
     return this.hx`
-    <div class="demo-layout mdl-layout mdl-layout--fixed-header mdl-js-layout mdl-color--grey-100">
-      <header class="demo-header mdl-layout__header mdl-layout__header--scroll mdl-color--grey-100 mdl-color-text--grey-800">
+    <div class="mdl-layout mdl-layout--fixed-header mdl-js-layout mdl-color--grey-100">
+      <header class="mdl-layout__header mdl-layout__header--scroll mdl-color--grey-100 mdl-color-text--grey-800">
         <div class="mdl-layout__header-row">
-          <span class="mdl-layout-title">Material Design Lite</span>
+          <span class="mdl-layout-title">YoV - Tiny ViewModel component for yo-yo</span>
           <div class="mdl-layout-spacer"></div>
           <div class="mdl-textfield mdl-js-textfield mdl-textfield--expandable">
             <label class="mdl-button mdl-js-button mdl-button--icon" for="search">
@@ -22,14 +44,24 @@ export default class Layout extends View {
           </div>
         </div>
       </header>
-      <div class="demo-ribbon"></div>
-      <main class="demo-main mdl-layout__content">
-        <div class="demo-container mdl-grid">
-          <div class="mdl-cell mdl-cell--2-col mdl-cell--hide-tablet mdl-cell--hide-phone"></div>
-          <div class="demo-content mdl-color--white mdl-shadow--4dp content mdl-color-text--grey-800 mdl-cell mdl-cell--8-col">
-            <div class="demo-crumbs mdl-color-text--grey-500">
-              ${content}
-            </div>
+      <main class="mdl-layout__content">
+        <div class="mdl-grid">
+          <div class="mdl-color--white mdl-shadow--4dp content mdl-color-text--grey-800 mdl-cell mdl-cell--6-col">
+            ${this.demoEl(props)}
+          </div>
+          <div class="mdl-color--white mdl-shadow--4dp content mdl-color-text--grey-800 mdl-cell mdl-cell--6-col">
+            ${content}
+          </div>
+
+          <div class="mdl-cell mdl-cell--12-col mdl-color--white mdl-shadow--4dp content mdl-color-text--grey-800">
+            <p class="mdl-typography--display-2-color-contrast" style="padding: 20px;">
+              Or open up your console and play with <code>form</code> and <code>layout</code> variables. These are the view instance we see above.
+
+              <pre class="mdl-color--grey-200 mdl-color-text--accent" style="padding-left: 15px;"><code>
+form.set('title', 'woot');
+layout.demo.set({ name: 'Morane', unread: 1456, total: 12569 });
+              </code></pre>
+            </p>
           </div>
         </div>
       </main>
